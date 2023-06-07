fc0a8bd0b2ec96c79950483496ab0c05de44b7f8
Daniel Kosc <daniel.kosc@cern.ch>
2023-06-07 11:30:21 +0200

    feat: input parameters parser and validator

--- service.py
+++ service.py
@@ -1,17 +1,32 @@
 import argparse
 
+from utils import check_address
+
 
 def parse_input_arguments():
+    """
+    Parse command line arguments from following format:
+    `python service.py --port 21000 --levels 1 --children 3 --parent "127.0.0.1:20000"`
+    In case of invalid input it throws error and print valid range, in case of missing option it returns default values
+
+    :return: object having 4 attributes:
+        -port: integer [10 000-60 000]
+            - default: 20 000
+        -levels: integer [0-4]
+            - default: 0
+        -children: integer [1-9]
+            - default: 3
+        -parent: string "<IP>:<port>"
+            - default: None
+    """
     parser = argparse.ArgumentParser(description='Process node input arguments.')
-    parser.add_argument('--port', dest='port', action='store', default=20000, help='port for root node')
-    parser.add_argument('--levels', dest='levels', action='store', default=4,
+    parser.add_argument('--port', dest='port', action='store', type=int, choices=range(10000, 60000), default=20000,
+                        help='port for root node')
+    parser.add_argument('--levels', dest='levels', action='store', type=int, choices=range(0, 5), default=0,
                         help='number of hierarchies/levels in three structure')
-    parser.add_argument('--children', dest='children', action='store', default=0,
+    parser.add_argument('--children', dest='children', action='store', type=int, choices=range(1, 10), default=3,
                         help='number of children per node except the leaves')
-    parser.add_argument('--parent', dest='parent', action='store', default=None,
+    parser.add_argument('--parent', dest='parent', action='store', type=check_address, default=None,
                         help='link to the parent node, keep empty')
     args = parser.parse_args()
     return args
-
-
-print(parse_input_arguments().port)
---
+++ utils.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def check_address(address):
+    if len(address.split(':')) != 2:
+        raise argparse.ArgumentTypeError("%s is not in correct IP:port format" % address)
+    ip = address.split(':')[0]
+    port = address.split(':')[1]
+    if len(ip.split('.')) != 4 or any(0 > int(octet) or int(octet) > 255 for octet in ip.split('.')):
+        raise argparse.ArgumentTypeError("%s is an invalid IP address value" % ip)
+    if int(port) < 10000 or int(port) >= 60000:
+        raise argparse.ArgumentTypeError("%s is out of range valid port values" % port)
+    return address
