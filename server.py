b02431c0f2949638fa3eb229b8bd95297bc02c3d
Daniel Kosc <daniel.kosc@cern.ch>
2023-06-07 15:07:29 +0200

    feat: endpoint headers

--- server.py
+++ server.py
@@ -13,3 +13,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/statemachine/state")
+def get_state() -> dict[str, str]:
+    return {"State": "MY_STATE"}
+
+
+@app.post("/statemachine/input")
+async def change_state(Start: str = None, Stop: str = None):
+    pass
+
+
+@app.post("/notifications")
+def notify():
+    pass
