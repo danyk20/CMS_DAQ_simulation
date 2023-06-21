ef0997e67982784da2b6629fd7278dbc86da3d1b
Daniel Kosc <daniel.kosc@cern.ch>
2023-06-21 15:25:42 +0200

    feat: initial topic MOM

--- receive.py
+++ receive.py
@@ -1,29 +1,34 @@
-import os
 import pika
-import sys
+
+from utils import get_configuration
+
+STATE_EXCHANGE = 'state_change'
+NOTIFICATION_EXCHANGE = 'state_notification'
+binding_key = '2.0.0.0.0'
+
+configuration: dict[str, str | dict[str, str | dict]] = get_configuration()
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=configuration['URL']['address']))
+channel = connection.channel()
+
+channel.exchange_declare(exchange=STATE_EXCHANGE, exchange_type='topic')
+channel.exchange_declare(exchange=NOTIFICATION_EXCHANGE, exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange=STATE_EXCHANGE, queue=queue_name, routing_key=binding_key)
+channel.queue_bind(exchange=NOTIFICATION_EXCHANGE, queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
 
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-
-    channel.queue_declare(queue='hello')
-
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
-
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
 
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
--- send.py
+++ send.py
@@ -1,11 +1,33 @@
 import pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+from utils import get_configuration
 
-channel.queue_declare(queue='hello')
+STATE_EXCHANGE = 'state_change'
+NOTIFICATION_EXCHANGE = 'state_notification'
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+configuration: dict[str, str | dict[str, str | dict]] = get_configuration()
+
+
+def post_state_change(new_state: str, routing_key: str):
+    send_message(new_state, routing_key, STATE_EXCHANGE)
+
+
+def post_state_notification(current_state: str, routing_key: str, sender_id: str):
+    send_message(sender_id + ":" + current_state, routing_key, NOTIFICATION_EXCHANGE)
+
+
+def send_message(message: str, routing_key: str, exchange_name: str):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=configuration['URL']['address']))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange=('%s' % exchange_name), exchange_type='topic')
+
+    channel.basic_publish(
+        exchange=exchange_name, routing_key=routing_key, body=str.encode(message))
+    # debug
+    print(" [x] Sent %r:%r" % (routing_key, message))
+    connection.close()
+
+
+post_state_change("hello world", '2.0.0.0.0')
