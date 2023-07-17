27c56bad4d8e3da1ce1853a05f4a3da5db81791d
Daniel Kosc <daniel.kosc@cern.ch>
2023-07-17 17:54:08 +0200

    feat: CI automated testing

--- test_node.py
+++ test_node.py
@@ -7,6 +7,28 @@ from model import Node, NodeAddress, State
 import pytest
 
 
+class TestNode:
+    def test_rpc_client_value(self):
+        init_node = generate_node(State.Initialisation)
+        white_envelope = utils.get_white_envelope('get_state')
+        chanel = ChannelStub()
+        init_node.on_request(ch=chanel, body=white_envelope, method=MethodStub(), props=PropertiesStub())
+        response = json.loads(chanel.blue_msg)
+        assert response == '{"state": "Initialisation"}'
+        assert chanel.routing_key == 'reply_to'
+        assert chanel.properties.correlation_id == 'correlation_id'
+
+    def test_rpc_client_duration(self):
+        init_node = generate_node(State.Initialisation)
+        white_envelope = utils.get_white_envelope('get_state')
+        chanel = ChannelStub()
+        start = time.time()
+        init_node.on_request(ch=chanel, body=white_envelope, method=MethodStub(), props=PropertiesStub())
+        end = time.time()
+        duration = end - start
+        assert 9 < duration < 11
+
+
 def generate_node(state: State, address: str = '127.0.0.0:20000', ) -> Node:
     node_add = NodeAddress(address)
     node = Node(node_add)
@@ -41,25 +63,3 @@ class PropertiesStub:
     def __init__(self):
         self.reply_to = 'reply_to'
         self.correlation_id = 'correlation_id'
-
-
-def test_rpc_client_value():
-    init_node = generate_node(State.Initialisation)
-    white_envelope = utils.get_white_envelope('get_state')
-    chanel = ChannelStub()
-    init_node.on_request(ch=chanel, body=white_envelope, method=MethodStub(), props=PropertiesStub())
-    response = json.loads(chanel.blue_msg)
-    assert response == '{"state": "Initialisation"}'
-    assert chanel.routing_key == 'reply_to'
-    assert chanel.properties.correlation_id == 'correlation_id'
-
-
-def test_rpc_client_duration():
-    init_node = generate_node(State.Initialisation)
-    white_envelope = utils.get_white_envelope('get_state')
-    chanel = ChannelStub()
-    start = time.time()
-    init_node.on_request(ch=chanel, body=white_envelope, method=MethodStub(), props=PropertiesStub())
-    end = time.time()
-    duration = end - start
-    assert 9 < duration < 11
