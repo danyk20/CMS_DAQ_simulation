b862e26e5cb6d920655af9a8c0b7792e7693b877
Daniel Kosc <daniel.kosc@cern.ch>
2023-08-29 15:13:49 +0200

    feat: Python 3.11 compatible

--- publisher.py
+++ publisher.py
@@ -6,17 +6,16 @@ import utils
 configuration: dict[str, str | dict[str, str | dict]] = utils.get_configuration()
 
 
-@asyncio.coroutine
-def new_task(exchange_name, routing_key, message):
+async def new_task(exchange_name, routing_key, message):
     try:
-        transport, protocol = yield from aioamqp.connect(host='127.0.0.1', port=5672, login='guest', password='guest')
+        transport, protocol = await aioamqp.connect(host='127.0.0.1', port=5672, login='guest', password='guest')
     except aioamqp.AmqpClosedConnection:
         print('Connection is closed! Message ' + str(message) + 'could not be sent!')
 
-    channel = yield from protocol.channel()
-    yield from channel.exchange_declare(exchange_name=exchange_name, type_name='topic')
+    channel = await protocol.channel()
+    await channel.exchange_declare(exchange_name=exchange_name, type_name='topic')
 
-    yield from channel.basic_publish(
+    await channel.basic_publish(
         payload=message.encode('utf-8'),
         exchange_name=exchange_name,
         routing_key=routing_key,
@@ -24,7 +23,7 @@ def new_task(exchange_name, routing_key, message):
             'delivery_mode': 2
         }
     )
-    yield from protocol.close()
+    await protocol.close()
     transport.close()
     if configuration['debug']:
         print(" [x] Sent message: %r -> %r" % (message, routing_key))
